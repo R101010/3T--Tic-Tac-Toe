dcc0455b6cbd5a8259984be635f16919750c112f
RR <RubenR42@icloud.com>
2020-02-07 03:10:05 -0800

    Add 3T board

---
+++ js/main.js
@@ -0,0 +1,21 @@
+/*----- constants -----*/
+p1-board = [];
+p2-board = [];
+var winningSets = [
+    [ a1, b1, c1],
+    [ a2, b2, c2],
+    [ a3, b3, c3],
+    [ a1, a2, a3],
+    [ b1, b2, b3],
+    [ c1, c2, c3],
+    [ a1, b2, c3],
+    [ c1, b2, a3],
+];
+/*----- app's state (variables) -----*/
+
+/*----- cached element references -----*/
+
+/*----- event listeners -----*/
+document.querySelector('table').addEventListener('click', );
+document.querySelector('button').addEventListener('click', reset);
+/*----- functions -----*/
